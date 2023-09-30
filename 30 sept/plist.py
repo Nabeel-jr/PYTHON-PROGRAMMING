35f5fac79898c19b470bc336998aaa01735933dc
Nabeel-jr <145667291+Nabeel-jr@users.noreply.github.com>
2023-09-30 16:06:16 +0530

    Add files via upload

---
+++ 30 sept/plist.py
@@ -0,0 +1,8 @@
+print("\n list of integers \n")
+list1=[-1,-2,-3,-4,1,2,3,4,5]
+for n in list1:
+ print(n,end=" ") 
+print("\n +ve integes are \n")
+for n in list1:
+    if n>0:
+     print(n,end=" ")
