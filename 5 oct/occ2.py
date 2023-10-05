c74ab911895d3d7a3a4fc9c406e60bee7586d578
Nabeel-jr <145667291+Nabeel-jr@users.noreply.github.com>
2023-10-05 19:42:17 +0530

    Add files via upload

---
+++ 5 oct/occ2.py
@@ -0,0 +1,11 @@
+
+lst=["jabbar","ganeshan","bapputty","hajiyar","ayamu","mayami","kunjayamu","ambili","ambu","ramani","abu"]
+count=0
+print(lst)
+a=input("enter the letter to find the occurance\n")
+for n in lst:
+  # z=n.split()
+   for f in n:
+        if f==a:
+            count+=1
+print("number of ",a," is:", count)
