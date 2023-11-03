c733c4fa3822571dc3389180f7fae8c34e60cecb
Nabeel-jr <145667291+Nabeel-jr@users.noreply.github.com>
2023-11-03 16:15:39 +0530

    Create perfecteven4.py

---
+++ Nov 3/perfecteven4.py
@@ -0,0 +1,20 @@
+import math
+n=int(input("\n enter the limit \n"))
+lst=[]
+for i in range(999,n):
+         z=math.sqrt(i)
+         if z.is_integer():
+            lst.append(str(i))
+print("all elements\n",lst)
+elst=[]
+for i in lst:
+    count=0
+    for z in i:
+        if int(z)%2==0:
+                count=count+1
+        else:
+            break
+    if count==4:
+        elst.append(int(i))
+                
+print("four even digit  perfect square\n"elst)    
