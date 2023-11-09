ea2fecf2e5e8d5296a54a98edb0f40326942dba3
Nabeel-jr <145667291+Nabeel-jr@users.noreply.github.com>
2023-11-09 16:31:00 +0530

    Add files via upload

---
+++ NOV 9/sumavgpow.py
@@ -0,0 +1,9 @@
+from nabeel import sum as s
+from nabeel import avg as a
+from nabeel import pow as p
+
+n=int(input("enter the number 1\n"))
+m=int(input("enter the number 2\n"))
+print("sum = ",s.sum(n,m))
+print("avg = ",a.avg(n,m))
+print("pow = ",p.pow(n,m))
